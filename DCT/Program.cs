31b9772ad282c1fb2dc0855a1a52f927155ec3c0
myniri <mykhailo.niri@gmail.com>
2024-04-07 10:56:11 +0300

    Updated swagger documentation: added title. description and annotations.

--- DCT/Program.cs
+++ DCT/Program.cs
@@ -5,6 +5,7 @@ using HackerNewsIntegration.Options;
 using HackerNewsIntegration.Services;
 using HttpClientHelper.Interfaces;
 using HttpClientHelper.Services;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -17,7 +18,19 @@ builder.Services.AddScoped<IHackerNewsService, HackerNewsService>();
 builder.Services.AddScoped<IHackerNewsIntegrationService, HackerNewsIntegrationService>();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Developer Coding Test",
+        Description = @"Bamboo-card - Developer Coding Test
+                        stories from the Hacker News API, as determined by their score, where
+                        n is specified by the caller to the API.",
+    });
+
+    c.EnableAnnotations();
+});
+
 builder.Services.AddLogging();
 
 var app = builder.Build();
